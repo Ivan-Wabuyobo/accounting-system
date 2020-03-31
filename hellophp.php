7c0ce436ca8be5e385a6d13eba65fbdfeb7eab0f
wabuyo ivan <wabuyoivan@gmail.com>
2020-03-31 22:15:59 +0300

    try this

--- hellophp.php
+++ hellophp.php
@@ -3,5 +3,8 @@
 echo 'hello world, welcome to php programming';
 echo "Today is " . date("Y/m/d");
 echo "The time is " . date("h:i:sa");
-
+#This is a comment, and
+#This is the second line of comment
+//This is a comment too. Each stye comments only
+print('An example with single line comments')
 ?>
